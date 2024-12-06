644030ed074de59518c512e00ba15b088abec754
koreakimbap <jh.kim.kor04@gmail.com>
2024-12-06 15:48:03 +0900

    Update deeplearningmodel.py

--- deeplearningmodel.py
+++ deeplearningmodel.py
@@ -1,17 +1,34 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout, Input
+from tensorflow.keras.layers import ConvLSTM2D, Flatten, Dense, Dropout, Input
 
 def build_model(input_shape):
     model = Sequential([
-        # Input(shape=(sequence_length, 10)),
         Input(shape=input_shape),  
-        LSTM(64, return_sequences=True),
+        ConvLSTM2D(filters=64, kernel_size=(3, 3), padding='same', return_sequences=False),
         Dropout(0.2),
-        LSTM(64),
-        Dropout(0.2),
+        Flatten(),
         Dense(50),  # Predicting the next 50 candles
     ])
     
     model.compile(optimizer='adam', loss='mse')
     
     return model
+
+
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import LSTM, Dense, Dropout, Input
+
+# def build_model(input_shape):
+#     model = Sequential([
+#         # Input(shape=(sequence_length, 10)),
+#         Input(shape=input_shape),  
+#         LSTM(64, return_sequences=True),
+#         Dropout(0.2),
+#         LSTM(64),
+#         Dropout(0.2),
+#         Dense(50),  # Predicting the next 50 candles
+#     ])
+    
+#     model.compile(optimizer='adam', loss='mse')
+    
+#     return model
